f600f8eb23451f3bad1e0c476bc72607cdb03888
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-02-11 11:59:18 +0000

    Added my original scene
    As I recall it.

---
+++ Energy Awarness Project/Assets/Nick/InteractionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InteractionScript : MonoBehaviour, IPointerClickHandler
+{
+    public List<GameObject> options;
+    public static bool clicked;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        MenuOptions.Show(options);
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].SetActive(true);
+        }
+        clicked = true;
+        Invoke("EndAction", 4);
+    }
+
+    void EndAction()
+    {
+        MenuOptions.Hide(options);
+        clicked = false;
+    }
+}
---
+++ Energy Awarness Project/Assets/Nick/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Components")]
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (InteractionScript.clicked) { agent.destination = transform.position; return; }
+        //Navigation for arrow keys
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            float horInput = Input.GetAxis("Horizontal");
+            float verInput = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horInput, 0f, verInput);
+            Vector3 moveDestination = transform.position + movement;
+            agent.SetDestination(moveDestination);
+        }
+        //Navigation for mouse click
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
