96e4ff379465a6ff2d8a8f30cdf3860ef8adc6a3
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2021-02-11 10:57:06 +0000

    movement

---
+++ Energy Awarness Project/Assets/Dimi/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+
+    public NavMeshAgent player;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+
+        }
+    }
+}
