e551ef9ae1ec773e12e83bb4c807fe19563d91ef
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-02-11 12:21:12 +0000

    Clickability of objects
    Made the clickability of objects distance based to player.

--- Energy Awarness Project/Assets/Nick/InteractionScript.cs
+++ Energy Awarness Project/Assets/Nick/InteractionScript.cs
@@ -7,15 +7,31 @@ public class InteractionScript : MonoBehaviour, IPointerClickHandler
 {
     public List<GameObject> options;
     public static bool clicked;
+    Transform player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerScript>().transform;
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        MenuOptions.Show(options);
-        for (int i = 0; i < options.Count; i++)
+        StopAllCoroutines();
+        if (Vector3.Magnitude(player.position - transform.position) < 2.5)
         {
-            options[i].SetActive(true);
+            MenuOptions.Show(options);
+            for (int i = 0; i < options.Count; i++)
+            {
+                options[i].SetActive(true);
+            }
+            clicked = true;
+            StartCoroutine(EndActions());
         }
-        clicked = true;
-        Invoke("EndAction", 4);
+    }
+
+    IEnumerator EndActions()
+    {
+        yield return new WaitForSeconds(4);
+        EndAction();
     }
 
     void EndAction()
