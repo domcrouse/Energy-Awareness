ea22848d0ace370d111e0193da7399666cbf9dfa
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2021-03-15 16:39:43 +0000

    Lights fixed

---
+++ Energy Awarness Project/Assets/Liam/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour
+{
+    public static int currentScene;
+
+    public void GoToLevel(int Level)
+    {
+        Time.timeScale = 1;
+        currentScene = Level;
+        SceneManager.LoadScene(Level);
+    }
+
+    public void ReturnToLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void AddScene(int level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
