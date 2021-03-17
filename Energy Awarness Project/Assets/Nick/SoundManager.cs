3d65deaf5f877897facd1389fe34fce40b0e471e
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-03-17 12:10:23 +0000

    Added SoundManager script.
    Made soundmanager script to help put sound effects and volume control into the game

---
+++ Energy Awarness Project/Assets/Nick/SoundManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    AudioSource source;
+    [Header("Sound Effects")]
+    public AudioClip lightSwitch;
+    public AudioClip deliverPoster;
+    public AudioClip thermostat;
+    public AudioClip rightAnswer;
+    public AudioClip wrongAnswer;
+    public AudioClip unlockDoor;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.volume = PlayerPrefs.GetFloat("Volume");
+        int isMute = PlayerPrefs.GetInt("Mute");
+        source.mute = isMute == 0 ? false : true;
+        
+    }
+
+    private void Start()
+    {
+        GameEvents.current.onTurnLightOn += SFXSwitch;
+        GameEvents.current.onDeliverPoster += SFXDeliver;
+        GameEvents.current.onChangeTemp += SFXThermo;
+        GameEvents.current.onCorrectAnswer += SFXRight;
+        GameEvents.current.onIncorrectAnswer += SFXWrong;
+        GameEvents.current.onUnlockDoor += SFXDoor;
+    }
+    private void OnDisable()
+    {
+        GameEvents.current.onTurnLightOn -= SFXSwitch;
+        GameEvents.current.onDeliverPoster -= SFXDeliver;
+        GameEvents.current.onChangeTemp -= SFXThermo;
+        GameEvents.current.onCorrectAnswer -= SFXRight;
+        GameEvents.current.onIncorrectAnswer -= SFXWrong;
+        GameEvents.current.onUnlockDoor -= SFXDoor;
+    }
+    public void MuteSound(bool toggle)
+    {
+        source.mute = toggle;
+        int isMute = toggle == true ? 1 : 0;
+        PlayerPrefs.SetInt("Mute", isMute);
+    }
+
+    public void ChangeVolume(System.Single value)
+    {
+        source.volume = value;
+        PlayerPrefs.SetFloat("Volume", value);
+    }
+
+    public void PauseAudio()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    void SFXSwitch(int id) { source.PlayOneShot(lightSwitch, 0.75f); }
+    void SFXDeliver(int id) { source.PlayOneShot(deliverPoster, 0.8f); }
+    void SFXThermo(int id) { source.PlayOneShot(thermostat, 0.6f); }
+    void SFXRight() { source.PlayOneShot(rightAnswer, 0.9f); }
+    void SFXWrong() { source.PlayOneShot(wrongAnswer, 0.9f); }
+    void SFXDoor(int id) { source.PlayOneShot(unlockDoor, 1); }
+}
